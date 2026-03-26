3f95f53da85eaea6328c47b14c76829f14676e9b
ardinger-msu <ardinger@msu.edu>
2026-03-26 14:50:20 +0000

    Proj 1 copied in

--- os/proc.h
+++ os/proc.h
@@ -7,6 +7,7 @@
 
 #define NPROC (512)
 #define FD_BUFFER_SIZE (16)
+#define MAX_SYSCALL_NUM (500)
 
 struct file;
 
@@ -30,7 +31,20 @@ struct context {
 	uint64 s11;
 };
 
+typedef enum {
+	UnInit,
+	Ready,
+	Running,
+	Exited
+} TaskStatus;
+
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
+
+struct TaskInfo {
+	TaskStatus status;
+	unsigned int syscall_times[MAX_SYSCALL_NUM];
+	int time;
+};
 
 // Per-process state
 struct proc {
@@ -45,6 +59,8 @@ struct proc {
 	struct proc *parent; // Parent process
 	uint64 exit_code;
 	struct file *files[FD_BUFFER_SIZE];
+
+	struct TaskInfo task_info
 };
 
 int cpuid();
